841593201739069095cb45ad9bae408a8fe8d7ff
sai-shashankN <116075549+sai-shashankN@users.noreply.github.com>
2025-04-25 16:33:16 +0530

    Update ProjectCards.js

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -2,39 +2,45 @@ import React from "react";
 import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import { CgWebsite } from "react-icons/cg";
-import { BsGithub } from "react-icons/bs";
+import { VscGithub } from "react-icons/vsc";
 
 function ProjectCards(props) {
   return (
     <Card className="project-card-view">
-      <Card.Img variant="top" src={props.imgPath} alt="card-img" />
+      {/* Show PDF if pdfLink exists, otherwise show image */}
+      {props.pdfLink ? (
+        <embed
+          src={props.pdfLink}
+          width="100%"
+          height="400px"
+          type="application/pdf"
+          style={{ borderRadius: "10px" }}
+        />
+      ) : (
+        <Card.Img variant="top" src={props.imgPath} alt="project-img" />
+      )}
+
       <Card.Body>
         <Card.Title>{props.title}</Card.Title>
-        <Card.Text style={{ textAlign: "justify" }}>
-          {props.description}
-        </Card.Text>
-        <Button variant="primary" href={props.ghLink} target="_blank">
-          <BsGithub /> &nbsp;
-          {props.isBlog ? "Blog" : "GitHub"}
-        </Button>
-        {"\n"}
-        {"\n"}
+        <Card.Text style={{ textAlign: "justify" }}>{props.description}</Card.Text>
 
-        {/* If the component contains Demo link and if it's not a Blog then, it will render the below component  */}
+        {/* GitHub/Certificate Link */}
+        {props.ghLink && (
+          <Button variant="primary" href={props.ghLink} target="_blank" style={{ marginRight: "10px" }}>
+            <VscGithub /> &nbsp;
+            {props.isBlog ? "Blog" : "View"}
+          </Button>
+        )}
 
-        {!props.isBlog && props.demoLink && (
-          <Button
-            variant="primary"
-            href={props.demoLink}
-            target="_blank"
-            style={{ marginLeft: "10px" }}
-          >
-            <CgWebsite /> &nbsp;
-            {"Demo"}
+        {/* Demo Link if it exists */}
+        {props.demoLink && (
+          <Button variant="primary" href={props.demoLink} target="_blank">
+            <CgWebsite /> &nbsp;Demo
           </Button>
         )}
       </Card.Body>
     </Card>
   );
 }
+
 export default ProjectCards;
