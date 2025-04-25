8590f544d02292a717f88c076e0e33a1396e6a95
sai-shashankN <116075549+sai-shashankN@users.noreply.github.com>
2025-04-25 06:53:23 +0530

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -19,37 +19,20 @@ function Home2() {
               LET ME <span className="purple"> INTRODUCE </span> MYSELF
             </h1>
             <p className="home-about-body">
-              I fell in love with programming and I have at least learnt
-              something, I think… 🤷‍♂️
+              I work across machine learning, game development, and cloud infrastructure — building things that are both functional and future-facing.
               <br />
-              <br />I am fluent in classics like
+              <br />
+              I write code in
               <i>
-                <b className="purple"> C++, Javascript and Go. </b>
+                <b className="purple"> Python, JavaScript, and C++</b>
               </i>
-              <br />
-              <br />
-              My field of Interest's are building new &nbsp;
+              , and enjoy working with tools like
               <i>
-                <b className="purple">Web Technologies and Products </b> and
-                also in areas related to{" "}
-                <b className="purple">
-                  Blockchain.
-                </b>
-              </i>
+                <b className="purple"> TensorFlow, Unity, React.js, and Firebase</b>
+              </i>.
               <br />
               <br />
-              Whenever possible, I also apply my passion for developing products
-              with <b className="purple">Node.js</b> and
-              <i>
-                <b className="purple">
-                  {" "}
-                  Modern Javascript Library and Frameworks
-                </b>
-              </i>
-              &nbsp; like
-              <i>
-                <b className="purple"> React.js and Next.js</b>
-              </i>
+              Most of what I build falls somewhere between intelligent systems and interactive experiences — I care about both how things work and how they feel.
             </p>
           </Col>
           <Col md={4} className="myAvtar">
@@ -62,42 +45,42 @@ function Home2() {
           <Col md={12} className="home-about-social">
             <h1>FIND ME ON</h1>
             <p>
-              Feel free to <span className="purple">connect </span>with me
+              Feel free to <span className="purple">connect</span> with me
             </p>
             <ul className="home-about-social-links">
               <li className="social-icons">
                 <a
-                  href="https://github.com/soumyajit4419"
+                  href="https://github.com/YOUR_GITHUB_USERNAME"
                   target="_blank"
                   rel="noreferrer"
-                  className="icon-colour  home-social-icons"
+                  className="icon-colour home-social-icons"
                 >
                   <AiFillGithub />
                 </a>
               </li>
               <li className="social-icons">
                 <a
-                  href="https://twitter.com/Soumyajit4419"
+                  href="https://twitter.com/YOUR_TWITTER_HANDLE"
                   target="_blank"
                   rel="noreferrer"
-                  className="icon-colour  home-social-icons"
+                  className="icon-colour home-social-icons"
                 >
                   <AiOutlineTwitter />
                 </a>
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.linkedin.com/in/soumyajit4419/"
+                  href="https://www.linkedin.com/in/YOUR_LINKEDIN_HANDLE"
                   target="_blank"
                   rel="noreferrer"
-                  className="icon-colour  home-social-icons"
+                  className="icon-colour home-social-icons"
                 >
                   <FaLinkedinIn />
                 </a>
               </li>
               <li className="social-icons">
                 <a
-                  href="https://www.instagram.com/soumyajit4419"
+                  href="https://www.instagram.com/YOUR_INSTAGRAM"
                   target="_blank"
                   rel="noreferrer"
                   className="icon-colour home-social-icons"
