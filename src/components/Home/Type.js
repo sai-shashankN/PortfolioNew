15c1bdf0ddff4a55d5be795a0e19a0d373e4a3c4
sai-shashankN <116075549+sai-shashankN@users.noreply.github.com>
2025-04-25 06:34:57 +0530

    Update Type.js

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -6,14 +6,15 @@ function Type() {
     <Typewriter
       options={{
         strings: [
-          "Software Developer",
-          "Freelancer",
-          "MERN Stack Developer",
-          "Open Source Contributor",
+          "Machine Learning Engineer",
+          "Game Developer",
+          "Cloud Engineer",
         ],
         autoStart: true,
         loop: true,
-        deleteSpeed: 50,
+        delay: 30,         // Faster typing
+        deleteSpeed: 20,   // Faster deletion
+        pauseFor: 1000,    // Shorter pause between phrases
       }}
     />
   );
