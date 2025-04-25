0e8b04700551f10678c237251d47db5eb0182f1c
sai-shashankN <116075549+sai-shashankN@users.noreply.github.com>
2025-04-25 06:44:28 +0530

    Update Type.js

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -3,7 +3,7 @@ import React from "react";
 function Type() {
   return (
     <div style={{ fontSize: "1.4rem", fontWeight: 500 }}>
-      <span style={{ color: "#00d1b2", animation: "fadeIn 1s" }}>Machine Learning Engineer</span>{' '}
+      <span style={{ color: "#00d1b2", animation: "fadeIn 1s" }}>AI Engineer</span>{' '}
       <span style={{ color: "#ff6b6b", animation: "fadeIn 1.4s" }}>• Game Developer</span>{' '}
       <span style={{ color: "#fcbf49", animation: "fadeIn 1.8s" }}>• Cloud Engineer</span>
     </div>
